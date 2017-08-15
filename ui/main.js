2990447f2accf4a895e73c545ca5d0b8fa070045
Abdul Kabeer <Kabeera987@gmail.com>
2017-08-15 13:52:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,34 +11,50 @@ button.onclick = function() {
       if (request.readyState=== XMLHttpRequest.DONE) {
           //Take some action
           if (request.status === 200){
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
+              var counter=request.responseText;
+              var span= document.getElementById('count');
+              span.innerHTML = counter.tostring();
           }
       }
         //Not done yet
     };
     
     //Make the request 
-    request.open('GET','http://kabeera987.imad.hasura-app.io/counter',true);
+    request.open('GET','http://kabeera987.imad.hasura-app.io/submit-name?name='+ name,true);
     request.send(null);
 };
 
-//SUbmit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick= function () {
-    //Make a request to the server and send the name 
+//Submit name 
+var nameInput= document.getElementById('name');
+var name= nameInput.vale;
+var submit= document.getElementById('submit_btn');
+submit.onclick = function () {
     
-    //Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++){
-        list += '<li>' + names[i] + '<li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //Creat a request object 
+    var request = new XMLHttpRequest();
+    
+    //Creat the response and store it in a variable 
+    request.onreadystatechange = function () {
+        if (request.readyState ===XMLHttpRequest.Done){
+            //Take some action
+            if (request.status ===200){
+              //Capture a list of names and render it as a list
+              var names = request.responseText;
+              names=JSON.parse(names);
+              var list = '';
+              for (var i=0; i<names,length; i++){
+                  list += '<li>' + names[i] + '</li>'
+                  
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+            }
+        }
+   //Not done yet
+    };
+    
+    //Make the request 
+    request.open('GET','http://kabeera987.imad.hasura-app.io/submit-name?name='+ name,true);
+    request.send(null);
 };
-
 
