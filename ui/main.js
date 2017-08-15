fd9b90a124da74fd6f9a491093a3721ed76b0940
Abdul Kabeer <Kabeera987@gmail.com>
2017-08-15 13:03:59 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,10 @@
 //Counter code 
 var button = document.getElementById('counter');
-var counter=0;
 
 button.onclick = function() {
     
     //Creat a request object
     var request= new XMLHttpRequest();
-    
     
     //Capture the response and store it in a variable
     request.onreadystatechange = function (){
@@ -25,4 +23,22 @@ button.onclick = function() {
     request.open('GET','http://kabeera987.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//SUbmit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick= function () {
+    //Make a request to the server and send the name 
     
+    //Capture a list of names and render it as a list
+    var names = ['name1', 'name2', 'name3'];
+    var list = '';
+    for (var i=0; i< names.length; i++){
+        list += '<li>' + names[i] + '<li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
+
+
