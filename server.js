d5c5611194157647a43aebce58ebea7bb64e7b48
Abdul Kabeer <Kabeera987@gmail.com>
2017-08-18 11:13:14 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
+var path = require('path'); 
+var pool= require('pg').Pool;
+
+var config = {
+    user:'kabeera987',
+    database:'kabeera987',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
+
+var app =express();
+app.use(morgan('combined'));
 
 var app = express();
 app.use(morgan('combined'));
@@ -80,6 +93,19 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    //make a select request
+    //and return a responce with results
+    pool.query('SELECT * FROM test', function (err,result){
+       if (err){ 
+           res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringfy(result));
+        }
+    });
 });
 
 var counter=0;
