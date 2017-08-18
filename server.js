c718d0a04d71bffef003a011cd124d203ff5b2d0
Abdul Kabeer <Kabeera987@gmail.com>
2017-08-18 11:32:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,12 +125,23 @@ app.get('/submit-name', function(req, res){//URL : /submit-name?name=xxxx
     
 });
 
-app.get('/:articleName', function(req, res){
+app.get('articels/:articleName', function(req, res){
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-
+    
+    pool.query("SELECT * FROM article WHERE title=+"+req.params.articelName, function(err, result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+              res.status(404).send('Articel not found');
+            }else{
+                var articleData = result.rows[0];
+                res.send(creatTemplate(articelData));
+            }
+    }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
